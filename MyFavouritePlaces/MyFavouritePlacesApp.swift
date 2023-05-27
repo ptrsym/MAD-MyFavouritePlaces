fc9a03dfc85629cdae65629a7cb09f3ba5c0b242
Peter <ptr.sym@gmail.com>
2023-05-28 01:00:58 +1000

    Initial Commit

---
+++ MyFavouritePlaces/MyFavouritePlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFavouritePlacesApp.swift
+//  MyFavouritePlaces
+//
+//  Created by Peter on 28/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyFavouritePlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
