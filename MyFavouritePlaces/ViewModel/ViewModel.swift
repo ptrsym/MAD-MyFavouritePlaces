bfe642bec974c5ad6a1824bd82d7f17dbbfec0d4
Peter <ptr.sym@gmail.com>
2023-05-31 05:58:15 +1000

    Restructuring files. created data model file to house core data extensions

--- MyFavouritePlaces/ViewModel/ViewModel.swift
+++ MyFavouritePlaces/ViewModel/ViewModel.swift
@@ -16,7 +16,9 @@ var defaultImage = Image(systemName: "photo").resizable()
 //dictionary to cache images once downloaded
 var downloadImage: [URL: Image] = [:]
 
-//saves context to persist data
+/// Saves the current context
+///
+/// This function allows data persistance using the CoreData functionality. It accesses the current working context and saves it.
 func saveData() {
     let context = PersistenceHandler.shared.container.viewContext
     do {
@@ -26,7 +28,12 @@ func saveData() {
     }
 }
 
-// class to handle interactions with the MapKit feature
+/// class to handle interactions with the MapKit feature
+///
+/// The `MapViewModel` class represents a view model for managing map-related data and functionality.
+/// It provides properties for storing and accessing information about a specific place, including its name, latitude, longitude, delta value, and region on the map.
+/// Use the `MapViewModel` class to configure and update the map view in your application, and observe the published properties to respond to changes in the map data.
+
 class MapViewModel: ObservableObject {
     @Published var place: Place?
     @Published var name: String
@@ -44,7 +51,11 @@ class MapViewModel: ObservableObject {
                                             CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0), span:
                                             MKCoordinateSpan(latitudeDelta: 100, longitudeDelta: 100))
     }
-    //string representation of stored latitude value. also validates latitude
+    /// A string represtation of the stored latitude value.
+    ///
+    /// This property provides a formatted string representing the latitude value of the location.
+    /// It also validates the latitude value when set.
+    ///
     var latStr:String {
         get{
             String(format: "%.5f", self.latitude)
@@ -57,8 +68,11 @@ class MapViewModel: ObservableObject {
             }
         }
     }
-    
-    // string represtation of stored longitude double. also validates longitude
+
+    /// string represtation of stored longitude double. also validates longitude
+    ///
+    /// This property provides a formatted string represting the longitude of the location.
+    /// It also validates the longitude when set.
     var longStr:String {
         get{
             String(format: "%.5f", self.longitude)
@@ -72,7 +86,12 @@ class MapViewModel: ObservableObject {
         }
     }
     
-    // links a place instance to an empty mapModel retrieved from environment
+    /// Updates the model with attributes based on the associated place
+    ///
+    /// This method updates each model attribute with the corresponding values stored within the passed Place object
+    /// so that map operations may reflect this place.
+    ///
+    /// - Parameter place: The 'Place' instance containing the attributes for the model to operate on
     func updateModel(_ place: Place){
         self.place = place
         self.name = place.name ?? "no name"
@@ -84,7 +103,11 @@ class MapViewModel: ObservableObject {
                                             MKCoordinateSpan(latitudeDelta: place.delta, longitudeDelta: place.delta))
     }
     
-    //loads the mapModel configuration back into its associated place instance and saves context
+ 
+    /// Updates the place instance associated with the model.
+    ///
+    /// This method sets the attributes of the associated place to those stored in the model from the result of operations done on the map.
+    /// It sets the values then saves the current context to persist changes.
     func updatePlace() {
         self.place?.longitude = self.region.center.longitude
         self.place?.latitude = self.region.center.latitude
@@ -93,13 +116,18 @@ class MapViewModel: ObservableObject {
         saveData()
     }
     
-    //updates stored long/lat values based on current orientation
+
+    /// Updates the stored latitude and longitude attributes with values based on the current region orientation of the map.
     func updateFromRegion(){
         self.longitude = region.center.longitude
         self.latitude = region.center.latitude
     }
     
-    //pans the map to the region specified in stored mapmodel attributes with an animation
+    
+    /// Sets the view region with an animation
+    ///
+    /// This method sets the region property specification to the currently stored latitude, longitude and delta values of the model
+    /// It is called after changes have been made to the attributes but the map view has not been centered on them.
     func setRegion(){
         withAnimation{
             region.center.latitude = self.latitude
@@ -163,133 +191,7 @@ class MapViewModel: ObservableObject {
     }
 
 }
-
-extension Place {
-    
-    // function to generate a thumbnail image based on current map orientation
-    func generateThumbnailImage() async -> UIImage?{
-        
-        // create options instances and configure to current place setting
-        let options = MKMapSnapshotter.Options()
-        options.region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude:
-                         self.latitude, longitude: self.longitude), span: MKCoordinateSpan(
-                         latitudeDelta: 0.1, longitudeDelta: 0.1))
-        options.mapType = .standard
-        options.size = CGSize(width: 30, height: 30)
-        
-        do{
-            // load options and take snapshot asynchronously
-            let snapshot = try await MKMapSnapshotter(options: options).start()
-            return snapshot.image
-        } catch {
-            print("Error creating map snapshot: \(error)")
-            return nil
-        }
-    }
-    // adds a new detail to the place relationship from the context
-    func addDetail(_ description:String) {
-        let context = PersistenceHandler.shared.container.viewContext
-        let newDetail = Detail(context: context)
-        newDetail.detail = description
-        newDetail.place = self
-        saveData()
-    }
-    
-    // attempt to retrieve an image from a dictionary of cached images if it exists, if not download the image located
-    // at the stored url attribute and add it to the downloadImage dictioanry. if no URl is stored or an error occurs
-    // return a default image
-    
-    func getImage() async -> Image {
-        guard let url = self.imgurl else {return defaultImage}
-        if let image = downloadImage[url] {return image}
-            do{
-                let (data, _) = try await URLSession.shared.data(from: url)
-                guard let uiimg = UIImage(data: data) else {return defaultImage}
-                let image = Image(uiImage: uiimg).resizable()
-                downloadImage[url]=image
-                return image
-            }catch {
-                print("error in downloading image \(error)")
-            }
-        
-        return defaultImage
-    }
-        // validates delta value and adds string retrieval
-        var strDelta:String {
-            get {
-                String(self.delta)
-            }
-            set {
-                if let doubleValue = Double(newValue){
-                    self.delta = doubleValue
-                } else {
-                    print("Invalid delta value \(newValue)")
-                }
-            }
-        }
-    
-    var strName:String {
-        get {
-            self.name ?? "no name"
-        }
-        set {
-            self.name = newValue
-        }
-    }
-    
-    //computed value to validate correct longitude entry
-    //gets a string representation of stored double
-    var strLongitude: String {
-        get {
-            String(format: "%.5f", self.longitude)
-        }
-        set {
-            if let doubleValue = Double(newValue), doubleValue >= -180.0, doubleValue <= 180.0 {
-                self.longitude = doubleValue
-            } else {
-                print("Invalid longitude value \(newValue)")
-            }
-        }
-    }
-    
-    //computed value to validate correct latitude entry
-    // gets a string representation of stored double value
-    var strLatitude:String{
-        get{
-            String(format: "%.5f", self.latitude)
-        }
-        set {
-            if let doubleValue = Double(newValue), doubleValue >= -90.0, doubleValue <= 90.0 {
-                self.latitude = doubleValue
-            } else {
-                print("Invalid latitude value \(newValue)")
-                }
-            }
-        }
-    
-    //computed value to fetch a string url from image or set url from string
-    var strUrl: String {
-        get {
-            self.imgurl?.absoluteString ?? ""
-        }
-        set {
-            guard let url = URL(string: newValue) else {return}
-            self.imgurl = url
-            }
-        }
-}
-    
-//extension Detail {
-//    var detailString:String {
-//        get {
-//            self.detail ?? "Add a description for your place"
-//        }
-//        set {
-//            self.detail = newValue
-//        }
-//    }
-//}
-
+		
 extension ContentView {
     // deletes a place entity from collection8
     func delPlace(index: IndexSet) {
@@ -326,9 +228,7 @@ extension DetailView {
         }
     }
 }
-
 extension MapView {
-    
     
     // callback function updating the local long/lat parameters from an address
     func checkAddress(){
@@ -373,3 +273,34 @@ extension MapView {
     }
     
 }
+
+
+let delta = 10.0
+
+class LocationManger: NSObject, CLLocationManagerDelegate, ObservableObject {
+    
+    @Published var region = MKCoordinateRegion()
+    
+    let manager = CLLocationManager()
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        region.span.longitudeDelta = delta
+        region.span.latitudeDelta = delta
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        locations.last.map{
+            region.center.latitude = $0.coordinate.latitude
+            region.center.longitude = $0.coordinate.longitude
+            print("lat: \($0.coordinate.latitude), long: \($0.coordinate.longitude)")
+        }
+    }
+}
+
+
+
+
+
+
